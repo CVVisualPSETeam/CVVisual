8d631f8eb94e81e4ef9b4fae71b35e59ffcb9051
Florian Weber <florian.weber@sfz-bw.de>
2014-01-07 23:45:50 +0100

    rename files, so they start with uppercase names

---
+++ include/opencv2/DebugDilate.hpp
@@ -0,0 +1,39 @@
+#ifndef CVVISUAL_DEBUG_DILATE_HPP
+#define CVVISUAL_DEBUG_DILATE_HPP
+
+#include <string>
+
+#include "CallData.hpp"
+#include "debugMode.hpp"
+
+
+// FIXME: replace this workaround
+namespace ocv {
+	class InputArray;
+	class OutputArray;
+}
+
+namespace cvv {
+
+
+namespace impl {
+	//implementation outside API
+	void debugDilate(const ocv::InputArray& in, const ocv::OutputArray& out, CallData data);
+} // namespace impl
+
+#ifdef CVVISUAL_DEBUGMODE
+static inline void debugDilate(const ocv::InputArray& in, const ocv::OutputArray& out,
+		impl::CallData data = impl::CallData()){
+	if(debugMode()) {
+		impl::debugDilate(in, out, data);
+	}
+}
+#else
+static inline void debugDilate(const ocv::InputArray&, const ocv::OutputArray&,
+		impl::CallData = impl::CallData()){}
+#endif
+
+} // namespace cvv
+
+
+#endif
