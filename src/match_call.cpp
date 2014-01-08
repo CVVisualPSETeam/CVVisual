05e45022f0765d798b7095eabed84b5dedd9403a
Florian Weber <florian.weber@sfz-bw.de>
2014-01-08 12:58:23 +0100

    add MatchCall

---
+++ src/match_call.cpp
@@ -0,0 +1,12 @@
+#include "match_call.hpp"
+
+namespace cvv { namespace impl {
+
+MatchCall::MatchCall(cv::InputArray img1, std::vector<cv::KeyPoint> keypoints1, cv::InputArray img2,
+		std::vector<cv::KeyPoint> keypoints2, std::vector<cv::DMatch> matches,
+		impl::CallData data, QString type ) :
+	Call(data, type), img1_{img1}, keypoints1_{std::move(keypoints1)}, img2_{img2},
+		keypoints2_{std::move(keypoints2)}, matches_{std::move(matches)} {}
+
+
+}} //namespaces cvv::impl
---
+++ src/match_call.hpp
@@ -0,0 +1,40 @@
+#ifndef CVVISUAL_MATCH_CALL_HPP
+#define CVVISUAL_MATCH_CALL_HPP
+
+#include <vector>
+#include <utility>
+
+#include "call.hpp"
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+
+namespace cvv { namespace impl {
+
+/**
+ * Contains all the calldata (= location, images and their keypoints).
+ */
+class MatchCall: public Call {
+public:
+	MatchCall(cv::InputArray img1, std::vector<cv::KeyPoint> keypoints1, cv::InputArray img2,
+			std::vector<cv::KeyPoint> keypoints2, std::vector<cv::DMatch> matches,
+			impl::CallData data, QString type );
+	
+	cv::InputArray img1() const {return img1_;}
+	cv::InputArray img2() const {return img2_;}
+	const std::vector<cv::KeyPoint>& keyPoints1() const {return keypoints1_;}
+	const std::vector<cv::KeyPoint>& keyPoints2() const {return keypoints2_;}
+	const std::vector<cv::DMatch>& matches() const {return matches_;}
+private:
+	cv::InputArray img1_;
+	std::vector<cv::KeyPoint> keypoints1_;
+	cv::InputArray img2_;
+	std::vector<cv::KeyPoint> keypoints2_;
+	std::vector<cv::DMatch> matches_;
+};
+
+
+}} //namespaces cvv::impl
+
+
+#endif
