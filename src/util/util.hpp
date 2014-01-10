583eed06dfd23224efffb4d57d4a8cc20c1d24c3
Florian Weber <florian.weber@sfz-bw.de>
2014-01-11 00:10:14 +0100

    extend documentation

--- src/util/util.hpp
+++ src/util/util.hpp
@@ -13,6 +13,12 @@
 
 namespace cvv { namespace util {
 
+/**
+ * @brief Creates a new Object of type T on the heap, managed by a std::unique_ptr.
+ * 
+ * @Note This function uses the naming-conventions of the STL instead of cvv because it actually
+ * is a backported function from C++14 that is intended to be in harmony with std::make_shared.
+ */
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args)
 {
@@ -21,7 +27,7 @@ std::unique_ptr<T> make_unique(Args&&... args)
 
 
 /**
- * Check whether a value compares equal to any value inside a container of comparable types.
+ * @brief Check whether a value compares equal to any value inside a container of comparable types.
  */
 template<typename ValueType, typename Container>
 bool isAnyOf(const ValueType& value, const Container& set)
@@ -30,7 +36,7 @@ bool isAnyOf(const ValueType& value, const Container& set)
 }
 
 /**
- * Overload for intializer-lists to enable usage like this: isAnyOf(3, {1,2,3})
+ * @brief Overload for intializer-lists to enable usage like this: isAnyOf(3, {1,2,3})
  */
 template<typename ValueType, typename Comparable>
 bool isAnyOf(const ValueType& value, const std::initializer_list<Comparable>& set)
@@ -66,19 +72,37 @@ public:
 	// there is no point in having a Reference to a temporary object:
 	Reference(T&&) = delete;
 	
+	/**
+	 * Get a reference to the referenced object.
+	 */
 	T& operator*() const {return *ptr;}
 	
 	T* operator->() const {return ptr;}
 	
+	/**
+	 * Get a reference to the referenced object.
+	 */
 	T& get() const {return *ptr;}
 	
+	/**
+	 * Get a pointer to the referenced object.
+	 */
 	T* getPtr() const {return ptr;}
 	
-	// comparing for equality is always usefull:
+	/**
+	 * @brief Compare to references for identity of the referenced object.
+	 * 
+	 * @note identity != equality: two references to two different ints that both happen to have
+	 * the value 1, will still compare unequal.
+	 */
 	bool friend operator==(const Reference& l, const Reference& r)
 	{
 		return l.ptr == r.ptr;
 	}
+	
+	/**
+	 * Dito.
+	 */
 	bool friend operator!=(const Reference& l, const Reference& r)
 	{
 		return l.ptr != r.ptr;
@@ -88,6 +112,11 @@ private:
 	T* ptr;
 };
 
+/**
+ * Create a cvv::util::Reference to an object. This is intended to be used for
+ * template-argument-deduction, so explicitly passing the template-argument should be considered
+ * undefined behaviour.
+ */
 template<typename T>
 Reference<T> makeRef(T& val)
 {
