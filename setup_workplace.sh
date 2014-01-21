28efbab4c97b5aa48e4a571f07422aedb6711ea4
Florian Weber <florian.weber@sfz-bw.de>
2014-01-21 20:03:28 +0100

    make setup_workspace parallel

--- setup_workplace.sh
+++ setup_workplace.sh
@@ -7,13 +7,13 @@ cd build
 mkdir release
 cd release
 cmake  -DCMAKE_BUILD_TYPE=Release ../..
-make
+make -j$(nproc)
 make test
 cd ..
 
 mkdir debug
 cd debug
 cmake  -DCMAKE_BUILD_TYPE=Debug ../..
-make
+make -j$(nproc)
 make test
 cd ..
