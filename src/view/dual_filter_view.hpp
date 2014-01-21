072d63caecaf3285c7a0b4053b29bb8a998f5a73
Clara <jola.scherer@web.de>
2014-01-21 14:10:17 +0100

    DualFilterView added

---
+++ src/view/dual_filter_view.hpp
@@ -0,0 +1,28 @@
+#ifndef CVVISUAL_DUAL_FILTER_VIEW_HPP
+#define CVVISUAL_DUAL_FILTER_VIEW_HPP
+
+#include "filter_view.hpp"
+
+namespace cvv {
+
+namespace qtutil{
+	class ZoomableImage{}; //dummy class
+
+	template<size_t bla, size_t blubb> //dummy class
+	class FilterSelectorWidget{};
+}
+
+namespace view {
+
+class DualFilterView: public FilterView
+{
+Q_OBJECT
+	private:
+		std::vector<cvv::qtutil::ZoomableImage> images;
+		cvv::qtutil::FilterSelectorWidget<2,1> selectorWidget;
+		cvv::qtutil::Accordion accordion;
+};
+
+}} //namespaces
+
+#endif
