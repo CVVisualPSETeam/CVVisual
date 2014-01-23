d1579ee611e1e40f00c077dd6d1d98bff00d5ddf
Florian Weber <florian.weber@sfz-bw.de>
2014-01-23 22:18:59 +0100

    create working manual test for debug_dilate

--- manual_test/basic_dilate/main.cpp
+++ manual_test/basic_dilate/main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include "../../include/opencv2/dilate.hpp"
 
 
-int main() {
-	cv::Mat input{100, 100, CV_8UC1, 0};
-	cv::Mat output{100, 100, CV_8UC1, 0};
+int main(int argc, char** argv) {
+	if(argc != 2)
+	{
+		return 1;
+	}
+	auto src = cv::imread(argv[1]);
+	auto elem = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9), cv::Point(4, 4));
+	cv::Mat dest;
+	cv::dilate(src, dest, elem);
 	
-	//cv::dilate(input, output, cv::Mat());
-
-	cvv::debugDilate(input, output, CVVISUAL_LOCATION);
+	cvv::debugDilate(src, dest, CVVISUAL_LOCATION);
 }
