3bd42370ddee373913113b384e524e55b75010ea
Florian Weber <florian.weber@sfz-bw.de>
2014-01-24 20:37:35 +0100

    restructure test-code

--- manual_test/basic_dilate/main.cpp
+++ manual_test/basic_dilate/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -5,15 +7,21 @@
 #include "../../include/opencv2/dilate.hpp"
 
 
-int main(int argc, char** argv) {
-	if(argc != 2)
-	{
-		return 1;
-	}
-	auto src = cv::imread(argv[1]);
+void actualWork(char* filename) {
+	auto src = cv::imread(filename);
 	auto elem = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9), cv::Point(4, 4));
 	cv::Mat dest;
 	cv::dilate(src, dest, elem);
 	
 	cvv::debugDilate(src, dest, CVVISUAL_LOCATION);
+	std::cout << "debugDilate returned, local destructors will run now" << std::endl;
+}
+
+int main(int argc, char** argv) {
+	if(argc != 2)
+	{
+		return 1;
+	}
+	actualWork(argv[1]);
+	std::cout << "local destructors have run, now all the global one will be executed" << std::endl;
 }
