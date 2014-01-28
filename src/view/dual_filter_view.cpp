b4d251aeb6e7d38f96f4b40e7bf6c60a4cb67eb1
Clara Scherer <jola.scherer@web.de>
2014-01-28 13:19:03 +0100

    Constructor for Dual Filter View

--- src/view/dual_filter_view.cpp
+++ src/view/dual_filter_view.cpp
@@ -1,10 +1,14 @@
-#include "filter_view.hpp"
+#include "dual_filter_view.hpp"
 
 namespace cvv
 {
 namespace view
 {
+	DualFilterView::DualFilterView(std::vector<cv::Mat> images, QWidget* parent)
+		: FilterView{parent}, rawImages_(images)
+	{
 
+	}
 
 
 }
