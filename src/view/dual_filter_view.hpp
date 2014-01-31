4c291a89bf1e46c1daa2b4648156f70304dbc111
Clara Scherer <jola.scherer@web.de>
2014-01-31 12:21:31 +0100

    added test for dual filter view, basic functionality for dual filter view

--- src/view/dual_filter_view.hpp
+++ src/view/dual_filter_view.hpp
@@ -1,5 +1,6 @@
 #ifndef CVVISUAL_DUAL_FILTER_VIEW_HPP
 #define CVVISUAL_DUAL_FILTER_VIEW_HPP
+
 //STD
 //OpenCV
 //QT
@@ -17,12 +18,17 @@ class DualFilterView: public FilterView
 Q_OBJECT
 	private:
 		std::vector<cv::Mat> rawImages_;
-		std::vector<cvv::qtutil::ZoomableImage> zoomImages_;
-		cvv::qtutil::FilterSelectorWidget<2,1> selectorWidget_;
-		cvv::qtutil::Accordion accordion_;
 
 	public:
 		DualFilterView(std::vector<cv::Mat> images, QWidget* parent = nullptr);
+};
+
+class DiffFilter: public qtutil::FilterFunctionWidget<2,1>
+{
+	const std::array<cv::Mat&,1>& applyFilter(const std::array<const cv::Mat&,2>& in,
+				const std::array<cv::Mat&,1>& out) const;
+
+	std::pair<bool, QString> checkInput(const std::array<const cv::Mat&,2>& in) const;
 };
 
 }} //namespaces
