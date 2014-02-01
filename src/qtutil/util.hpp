beecbcdab1fd4e2abcda2be527f13d0f1ff24da4
raphael grimm (ufdrv) <ufdrv@student.kit.edu>
2014-02-01 15:25:31 +0100

    some changes and functions

--- src/qtutil/util.hpp
+++ src/qtutil/util.hpp
@@ -10,6 +10,9 @@
 
 namespace cvv { namespace qtutil {
 
+/**
+ * @brief Represents the staus of an image conversion.
+ */
 enum class ImageConversionResult
 {
 	SUCCESS,
@@ -21,10 +24,23 @@ enum class ImageConversionResult
 	MAT_UNSUPPORTED_DEPTH
 };
 
+/**
+ * @brief Converts a cv::Mat to a QImage.
+ * @param mat The mat to convert.
+ * @param skipFloatRangeTest If true Mats with floating types will be checked
+ *  wheather all values are within [0,1].
+ * @return The status of the conversion and the converted mat.
+ */
 std::pair<ImageConversionResult,QImage>convertMatToQImage(const cv::Mat &mat,
 							bool skipFloatRangeTest=0);
 
-
+/**
+ * @brief Converts a cv::Mat to a QPixmap.
+ * @param mat The mat to convert.
+ * @param skipFloatRangeTest If true Mats with floating types will be checked
+ *  wheather all values are within [0,1].
+ * @return The status of the conversion and the converted mat.
+ */
 std::pair<ImageConversionResult,QPixmap>  convertMatToQPixmap(const cv::Mat &mat,
 							bool skipFloatRangeTest=0);
 
@@ -33,6 +49,15 @@ std::pair<ImageConversionResult,QPixmap>  convertMatToQPixmap(const cv::Mat &mat
  */
 QSet<QString> createStringSet(QString string);
 
+/**
+ * @brief Returns a string containing the type of the mat.
+ * @param mat The mat.
+ * @return A string containing the type of the mat.
+ * (first = flase when the depth is unknown)
+ */
+std::pair<bool, QString> typeToQString(const cv::Mat& mat);
+
+QString conversionResultToString(const ImageConversionResult& result);
 }}
 
 #endif
