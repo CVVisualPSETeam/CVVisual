f6bc3922ef7884785aed273a6564b6ff4424b908
Florian Weber <florian.weber@sfz-bw.de>
2014-02-10 14:14:23 +0100

    fix indentation, add debug-info and fix bugs

--- src/gui/overview_group_subtable.cpp
+++ src/gui/overview_group_subtable.cpp
@@ -13,75 +13,77 @@
 #include "overview_table.hpp"
 #include "../controller/view_controller.hpp"
 
+#include "../dbg/dbg.hpp"
+
 namespace cvv { namespace gui {
 
 OverviewGroupSubtable::OverviewGroupSubtable(util::Reference<controller::ViewController> controller,
-                                             OverviewTable *parent,
-                                             stfl::ElementGroup<OverviewTableCollumn> group):
-    controller{controller}, parent{parent}, group{std::move(group)}
+		 OverviewTable *parent,
+		 stfl::ElementGroup<OverviewTableCollumn> group):
+	controller{controller}, parent{parent}, group{std::move(group)}
 {
-    controller->setDefaultSetting("overview", "imgsize", QString::number(100));
-    initUI();
+	controller->setDefaultSetting("overview", "imgsize", QString::number(100));
+	initUI();
 }
 
 void OverviewGroupSubtable::initUI()
 {
 	controller->setDefaultSetting("overview", "imgzoom", "30");
 	qTable = new QTableWidget(this);
-    qTable->setSelectionBehavior(QAbstractItemView::SelectRows);
-    qTable->setSelectionMode(QAbstractItemView::SingleSelection);
+	qTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+	qTable->setSelectionMode(QAbstractItemView::SingleSelection);
 	auto verticalHeader = qTable->verticalHeader();
-    verticalHeader->setVisible(false);
+	verticalHeader->setVisible(false);
 	auto horizontalHeader = qTable->horizontalHeader();
-    horizontalHeader->setSectionResizeMode(QHeaderView::ResizeToContents);
+	horizontalHeader->setSectionResizeMode(QHeaderView::ResizeToContents);
 	horizontalHeader->setStretchLastSection(false);
 	qTable->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
 	connect(qTable, SIGNAL(cellDoubleClicked(int, int)), this, SLOT(rowClicked(int,int)));
 	qTable->setContextMenuPolicy(Qt::CustomContextMenu);
 	connect(qTable, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(customMenuRequested(QPoint)));
-    auto *layout = new QVBoxLayout;
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->addWidget(qTable);
-    setLayout(layout);
+	auto *layout = new QVBoxLayout;
+	layout->setContentsMargins(0, 0, 0, 0);
+	layout->addWidget(qTable);
+	setLayout(layout);
 	updateUI();
 }
 
 void OverviewGroupSubtable::updateUI(){
-    int imgSize = controller->getSetting("overview", "imgzoom").toInt() * width() / 400;
-    QStringList list{};
-    list << "ID";
-    size_t maxImages = 0;
-    for (auto element : group.getElements ())
-    {
-        if (maxImages < element.call()->matrixCount())
-        {
-            maxImages = element.call()->matrixCount();
-        }
-    }
-    if (parent->isShowingImages())
-    {
-        for (auto element : group.getElements ())
-    	{
-       	 if (maxImages < element.call()->matrixCount())
-        	{
-        	    maxImages = element.call()->matrixCount();
-    	    }
-	    }
+	int imgSize = controller->getSetting("overview", "imgzoom").toInt() * width() / 400;
+	QStringList list{};
+	list << "ID";
+	size_t maxImages = 0;
+	for (auto element : group.getElements ())
+	{
+		if (maxImages < element.call()->matrixCount())
+		{
+			maxImages = element.call()->matrixCount();
+		}
+	}
+	if (parent->isShowingImages())
+	{
+		for (auto element : group.getElements ())
+		{
+		if (maxImages < element.call()->matrixCount())
+			{
+				maxImages = element.call()->matrixCount();
+			}
+		}
 		for (size_t i = 0; i < maxImages; i++)
-        {
-            list << QString("Image ") + QString::number(i + 1);
-        }
-    }
-    list << "Description" << "Function" << "File" << "Line" << "Type";
-    qTable->setRowCount(group.size());
-    qTable->setColumnCount(list.size());
-    qTable->setHorizontalHeaderLabels(list);
+		{
+			list << QString("Image ") + QString::number(i + 1);
+		}
+	}
+	list << "Description" << "Function" << "File" << "Line" << "Type";
+	qTable->setRowCount(group.size());
+	qTable->setColumnCount(list.size());
+	qTable->setHorizontalHeaderLabels(list);
 	int rowHeight = std::max(imgSize, qTable->fontMetrics().height() + 5);
-    for (size_t i = 0; i < group.size(); i++)
-    {
-        group.get(i).addToTable(qTable, i, parent->isShowingImages(), maxImages, imgSize, imgSize);
-        qTable->setRowHeight(i, rowHeight);
-    }
+	for (size_t i = 0; i < group.size(); i++)
+	{
+		group.get(i).addToTable(qTable, i, parent->isShowingImages(), maxImages, imgSize, imgSize);
+		qTable->setRowHeight(i, rowHeight);
+	}
 	auto header = qTable->horizontalHeader();
 	header->setSectionResizeMode(0, QHeaderView::ResizeToContents);
 	for (size_t i = 1; i < maxImages + 1; i++)
@@ -98,8 +100,8 @@ void OverviewGroupSubtable::updateUI(){
 
 void OverviewGroupSubtable::rowClicked(int row, int collumn)
 {
-    (void)collumn;
-    size_t tabId = group.get(row).id();
+	(void)collumn;
+	size_t tabId = group.get(row).id();
 	controller->showAndOpenCallTab(tabId);
 }
 
@@ -107,19 +109,21 @@ void OverviewGroupSubtable::customMenuRequested(QPoint location)
 {
 	QMenu *menu = new QMenu(this);
 	connect(menu, SIGNAL(triggered(QAction*)), this, SLOT(customMenuAction(QAction*)));
-    auto windows = controller->getTabWindows();
-    menu->addAction(new QAction("Open in new window", this));
+	auto windows = controller->getTabWindows();
+	menu->addAction(new QAction("Open in new window", this));
 	for (auto window : windows)
 	{
+		// TODO: is this code really the best we can come up with?
 		menu->addAction(new QAction(QString("Open in '%1'").arg(
 						window->windowTitle()), this));
 	}
-    menu->addAction(new QAction("Remove call", this));
+	menu->addAction(new QAction("Remove call", this));
 	QModelIndex index = qTable->indexAt(location);
 	int row = index.row();
 	QString idStr = qTable->item(row, 0)->text();
 	currentCustomMenuCallTabId = idStr.toInt();  
 	menu->popup(qTable->viewport()->mapToGlobal(location));
+	TRACEPOINT;
 }
 
 void OverviewGroupSubtable::customMenuAction(QAction *action)
@@ -130,7 +134,7 @@ void OverviewGroupSubtable::customMenuAction(QAction *action)
 	}
 	QString actionText = action->text();
 	auto windows = controller->getTabWindows();
-	if (actionText == QString("Open in new window"))
+	if (actionText == "Open in new window")
 	{
 		controller->moveCallTabToNewWindow(currentCustomMenuCallTabId);
 		return;
@@ -144,6 +148,7 @@ void OverviewGroupSubtable::customMenuAction(QAction *action)
 	}
 	for (auto window : windows)
 	{
+		//TODO: see above: is string-compare really the best solution to this?
 		if (actionText == QString("Open in '%1'").arg(window->windowTitle()))
 		{
 			controller->moveCallTabToWindow(currentCustomMenuCallTabId, window->getId());
@@ -156,7 +161,7 @@ void OverviewGroupSubtable::customMenuAction(QAction *action)
 void OverviewGroupSubtable::resizeEvent(QResizeEvent *event)
 {
 	(void)event;
-	updateUI();		
+	updateUI();
 }
 
 }}
