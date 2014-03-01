8a1414d62228189336d50e38a9bc8c0ac737178c
Florian Weber <florian.weber@sfz-bw.de>
2014-03-01 18:31:08 +0100

    add train-flag for matches

--- src/impl/dmatch.cpp
+++ src/impl/dmatch.cpp
@@ -10,10 +10,10 @@ void debugDMatch(
 		cv::InputArray img1, std::vector<cv::KeyPoint> keypoints1,
 		cv::InputArray img2, std::vector<cv::KeyPoint> keypoints2,
 		std::vector<cv::DMatch> matches, const CallMetaData& data,
-		const char* description, const char* view)
+		const char* description, const char* view, bool useTrainDescriptor)
 {
 	debugMatchCall(img1, std::move(keypoints1), img2, std::move(keypoints2), std::move(matches),
-			data, description, view);
+			data, description, view, useTrainDescriptor);
 }
 
 }} //namespaces
