543e40fa0e2a6db89e2b16cc66e1206855881ce5
Florian Weber <florian.weber@sfz-bw.de>
2014-03-01 16:51:19 +0100

    extend documentation

--- src/extension_api/api.hpp
+++ src/extension_api/api.hpp
@@ -15,15 +15,29 @@
 namespace cvv { namespace extend {
 using FilterViewFactory =
 	std::function<std::unique_ptr<cvv::view::FilterView>(const std::vector<cv::Mat>&, QWidget*)>;
+/**
+ * @brief Introduces a new filter-view.
+ */
 void addFilterView(const QString name, FilterViewFactory factory);
 
 using MatchViewFactory = gui::MatchCallTab::MatchViewBuilder;
+/**
+ * @brief Introduces a new match-view.
+ */
 void addMatchView(const QString name, MatchViewFactory factory);
 
 using TabFactory = controller::TabFactory;
+/**
+ * @brief Introduces a new call-type.
+ * @param factory A function that recieves a reference to a call and should return the appropriate
+ * window.
+ */
 void addCallType(const QString name, TabFactory factory);
 
 template<std::size_t In, std::size_t Out, class Filter>
+/**
+ * @brief Introduces a new filter for the filter-selector-widget.
+ */
 bool registerFilter(const QString& name)
 {
 	return cvv::qtutil::registerFilter<In, Out, Filter>(name);
