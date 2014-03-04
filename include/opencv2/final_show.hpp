e20b428326f8b91f924049f710ee3e395f2a67a2
Florian Weber <florian.weber@sfz-bw.de>
2014-03-04 20:50:02 +0100

    make multiple callls to finalCall() legal (restart everything)

--- include/opencv2/final_show.hpp
+++ include/opencv2/final_show.hpp
@@ -14,12 +14,14 @@ void finalShow();
 /**
  * @brief Passes the control to the debug-window for a last time.
  *
- * This function must be called EXACTLY once if there was any prior debug-call.
+ * This function must be called once if there was any prior debug-call. After that all debug-data
+ * are freed.
  *
  * If there was no prior call it may be called once in which case it returns
- *without opening a window.
+ * without opening a window.
  *
  * In either case no further debug-calls must be made (undefined behaviour!!).
+ *
  */
 inline void finalShow()
 {
