46d98b41a5fb8c7aa2ea01aa49c92fc67b1ace10
Clara Scherer <jola.scherer@web.de>
2014-03-08 17:59:57 +0100

    add comments to manual filter view tests

--- manual_test/defaultfilterviewtest/main.cpp
+++ manual_test/defaultfilterviewtest/main.cpp
@@ -9,6 +9,30 @@
 
 #include "../../src/view/defaultfilterview.hpp"
 
+/**
+ * DefaultFilterView Test:
+ * Give images as program arguments.
+ *
+ * A window should open with an accordion menu on the left and as many images as
+ * you passed to the test on the right.
+ 
+ * The accordion menu should consist of the following collapsables:
+ * - Zoom sychronization:
+ *		When opened one plus as many checkboxes as there are images should be 
+ *		displayed. One for every image and one to indicate that no zoom
+ *		synchronization is selected.
+ *		If one image is selected, zooming or scrolling in this image is mapped to
+ *		the other image so they all show the same section and have the same zoom
+ *		level. Zooming or scrolling in a non-selected image only changes this image,
+ *		as it would if no zoom synchronization was selected.
+ * - Image Information x:
+ *		For each image there is a widget which displays information about the image.
+ * 		Zooming in and out of an image with both the spin box in the image information
+ * 		and (Shift) + Strg + Mouse Wheel should work (Shift: slower).
+ * 		Unchecking "Show image" should make it disappear, checking make it reappear.
+ * 		The information about the image in the collapsable should be correct.
+ * 		"Show full image" should work.
+ */
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
