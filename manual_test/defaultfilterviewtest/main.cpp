e721cee67ef6c1b96babcd66c34bbfc886cccda0
Clara Scherer <jola.scherer@web.de>
2014-03-08 17:58:31 +0100

    change defaultFilterView and SingleFilterView Test

--- manual_test/defaultfilterviewtest/main.cpp
+++ manual_test/defaultfilterviewtest/main.cpp
@@ -1,34 +1,31 @@
-#include <QApplication>
-#include <QImage>
 
 #include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <QApplication>
+#include <QImage>
 
 #include "../../src/view/defaultfilterview.hpp"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-
+	
+	if (argc < 2)
+	{
+		std::cerr << "Only execute this with filenames of images as "
+		             "arguments!" << std::endl;
+		return -1;
+	}
 	std::vector<cv::Mat> imagelist;
-
-	cv::Mat whitemat = cv::Mat{ 100, 100, CV_8U };
-	cv::Mat blackmat = cv::Mat{ 100, 100, CV_8U };
-	cv::Mat colormat1 = cv::Mat{ 100, 100, CV_8U };
-	cv::Mat colormat2 = cv::Mat{ 100, 100, CV_8U };
-	cv::Mat colormat3 = cv::Mat{ 100, 100, CV_8U };
-
-	whitemat = cv::Scalar(255, 255, 255);
-	blackmat = cv::Scalar(0, 0, 0);
-	colormat1 = cv::Scalar(255, 0, 0);
-	colormat2 = cv::Scalar(0, 255, 0);
-	colormat3 = cv::Scalar(0, 0, 255);
-
-	imagelist.push_back(whitemat);
-	imagelist.push_back(blackmat);
-	imagelist.push_back(colormat1);
-	imagelist.push_back(colormat2);
-	imagelist.push_back(colormat3);
-
+	
+	for(int i = 1; i < argc; i++)
+	{
+		auto src = cv::imread(argv[i]);
+		imagelist.push_back(src);
+	}
 	cvv::view::DefaultFilterView dfv{ imagelist, nullptr };
 
 	dfv.show();
--- manual_test/singlefilterviewtest/main.cpp
+++ manual_test/singlefilterviewtest/main.cpp
@@ -30,32 +30,8 @@ int main(int argc, char *argv[])
 	cvv::impl::initializeFilterAndViews();
 	std::vector<cv::Mat> vec{ src, dest };
 	cvv::view::SingleFilterView view{ vec };
-	view.setWindowTitle("Dual Filter View Test");
+	view.setWindowTitle("Single Filter View Test");
 	view.show();
-	/*
-	std::vector<cv::Mat> imagelist;
-
-	cv::Mat whitemat = cv::Mat{ 100, 100, CV_8UC3 };
-	cv::Mat blackmat = cv::Mat{ 100, 100, CV_8UC3 };
-	cv::Mat colormat1 = cv::Mat{ 100, 100, CV_8UC3 };
-	cv::Mat colormat2 = cv::Mat{ 100, 100, CV_8UC3 };
-	cv::Mat colormat3 = cv::Mat{ 100, 100, CV_8UC3 };
-
-	whitemat = cv::Scalar(255, 255, 255);
-	blackmat = cv::Scalar(0, 0, 0);
-	colormat1 = cv::Scalar(255, 0, 0);
-	colormat2 = cv::Scalar(0, 255, 0);
-	colormat3 = cv::Scalar(0, 0, 255);
-
-	imagelist.push_back(whitemat);
-	imagelist.push_back(blackmat);
-	imagelist.push_back(colormat1);
-	imagelist.push_back(colormat2);
-	imagelist.push_back(colormat3);
-	*/
 	
-	cvv::view::SingleFilterView dfv{vec};
-	dfv.setWindowTitle("Single Filter View Test");
-	dfv.show();
 	return a.exec();
 }
