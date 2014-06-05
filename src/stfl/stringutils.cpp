464b02dc5d47ddc2c1f3c9fcdf2a3f92b31a83a6
Johannes Bechberger <me@mostlynerdless.de>
2014-06-05 17:23:28 +0200

    Remove unused phonetic algorithm

--- src/stfl/stringutils.cpp
+++ src/stfl/stringutils.cpp
@@ -11,15 +11,6 @@ namespace cvv
 namespace stfl
 {
 
-int stringEquality(const QString &str1, const QString &str2)
-{
-	if (isSingleWord(str1) && isSingleWord(str2))
-	{
-		return phoneticEquality(str1, str2);
-	}
-	return editDistance(str1, str2);
-}
-
 size_t editDistance(const QString &str1, const QString &str2)
 {
 	const unsigned len1 = str1.size();
@@ -49,107 +40,6 @@ size_t editDistance(const QString &str1, const QString &str2)
 	}
 	return prevCol[len2];
 }
-
-int phoneticEquality(const QString &word1, const QString &word2)
-{
-	if (word1 == word2)
-	{
-		return 0;
-	}
-	return editDistance(nysiisForWord(word1), nysiisForWord(word2)) + 1;
-}
-
-QString nysiisForWord(QString word)
-{
-	static std::map<QString, QString> replacements = { { "MAC", "MCC" },
-		                                           { "KN", "NN" },
-		                                           { "K", "C" },
-		                                           { "PH", "FF" },
-		                                           { "PF", "FF" },
-		                                           { "SCH", "SSS" } };
-	static std::map<QString, QString> replacements2 = { { "EE", "Y" },
-		                                            { "IE", "Y" },
-		                                            { "DT", "D" },
-		                                            { "RT", "D" },
-		                                            { "NT", "D" },
-		                                            { "ND", "D" } };
-	static std::map<QString, QString> replacements3 = { { "EV", "AF" },
-		                                            { "Ü", "A" },
-		                                            { "Ö", "A" },
-		                                            { "Ä", "A" },
-		                                            { "O", "G" },
-		                                            { "Z", "S" },
-		                                            { "M", "N" },
-		                                            { "KN", "N" },
-		                                            { "K", "C" },
-		                                            { "SCH", "SSS" },
-		                                            { "PH", "FF" } };
-
-	if (word.isEmpty())
-	{
-		return "";
-	}
-
-	QString code;
-	word = word.toUpper();
-
-	replaceIfStartsWith(word, replacements);
-
-	replaceIfEndsWith(word, replacements2);
-	code.append(word[0]);
-	word = word.right(word.size() - 1);
-
-	while (word.size() > 0)
-	{
-		if (isVowel(word[0]))
-			word[0] = QChar('A');
-		replaceIfStartsWith(word, replacements);
-		if (!(word.startsWith("H") &&
-		      (!isVowel(code[code.size() - 1]) ||
-		       (word.size() >= 2 && !isVowel(word[1])))) &&
-		    !(word.startsWith("W") && isVowel(code[code.size() - 1])))
-		{
-			if (word[0] != code[code.size() - 1])
-			{
-				code.append(word[0]);
-			}
-		}
-		word = word.right(word.size() - 1);
-	}
-	if (code.endsWith("S"))
-	{
-		code = code.left(code.size() - 1);
-	}
-	if (code.endsWith("AY"))
-	{
-		code = code.right(code.size() - 1);
-		code[code.size() - 1] = QChar('Y');
-	}
-	else if (code.endsWith("A"))
-	{
-		code = code.left(code.size() - 1);
-	}
-	code = removeRepeatedCharacters(code);
-	return code;
-}
-
-QString nysiisForWordCached(const QString &word)
-{
-	static std::map<QString, QString> cache;
-	if (word.isEmpty())
-		return "";
-	if (cache.count(word))
-	{
-		return cache[word];
-	}
-	else
-	{
-		QString code = nysiisForWord(word);
-		cache[word] = code;
-		return code;
-	}
-}
-
 QString removeRepeatedCharacters(const QString &str)
 {
 	if (str.isEmpty())
