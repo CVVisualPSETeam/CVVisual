d8ff5770652ea78031bd2f6956310ed784f1d720
raphael grimm (ufdrv) <ufdrv@student.kit.edu>
2014-06-08 11:45:44 +0200

    added example for a filter in maual_test/filters
    (works with all images)

--- manual_test/filters/main.cpp
+++ manual_test/filters/main.cpp
@@ -1,3 +1,51 @@
+#include "../../src/extension_api/api.hpp"
+
+#include <QDoubleSpinBox>
+#include <QVBoxLayout>
+
+class MultiplyFilterWidget : public cvv::qtutil::FilterFunctionWidget<1, 1>
+{
+public:
+	using InputArray = FilterFunctionWidget<1, 1>::InputArray;
+	using OutputArray = FilterFunctionWidget<1, 1>::OutputArray;
+
+	MultiplyFilterWidget(QWidget *parent = nullptr):
+		FilterFunctionWidget<1, 1>{ parent }, mult_{ nullptr }
+	{
+		auto lay = cvv::util::make_unique<QVBoxLayout>();
+		auto spin = cvv::util::make_unique<QDoubleSpinBox >();
+		mult_ = *spin;
+
+		spin->setRange(0, 10);
+		spin->setSingleStep(0.1);
+
+		QObject::connect(
+			spin.get(),
+			SIGNAL(valueChanged(double)),
+			&(this->signalFilterSettingsChanged()),
+			SIGNAL(signal())
+		);
+
+		lay->addWidget(spin.release());
+		setLayout(lay.release());
+	}
+
+	virtual void applyFilter(InputArray in, OutputArray out) const override
+	{
+		out.at(0).get() = in.at(0).get().clone() * (mult_->value());
+	}
+
+
+	virtual std::pair<bool, QString> checkInput(InputArray) const override
+	{
+		return {true,""};
+	}
+
+private:
+	cvv::util::ObserverPtr<QDoubleSpinBox> mult_;
+};
+
+
 #include <iostream>
 
 #include <opencv2/core/core.hpp>
@@ -11,7 +59,7 @@ void dilateFile(char *filename)
 {
 	auto src = cv::imread(filename);
 	auto elem = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(9, 9),
-	                                      cv::Point(4, 4));
+					      cv::Point(4, 4));
 	cv::Mat dest;
 
 	cv::dilate(src, dest, elem);
@@ -27,7 +75,7 @@ void dilateFile(char *filename)
 /**
  * @brief This test filters  all the provided images with erode(), dilate(), Sobel() and morphologyEx()
  * and visualizes these filters with the debug-framework.
- * 
+ *
  * Expected behaviour:
  * * A Mainwindow should open that shows an overview-table containing the first image and it's dilated
  *   version.
@@ -40,6 +88,8 @@ void dilateFile(char *filename)
  */
 int main(int argc, char **argv)
 {
+	cvv::extend::registerFilter<1,1,MultiplyFilterWidget>("mult");
+
 	if (argc == 1)
 	{
 		std::cerr
